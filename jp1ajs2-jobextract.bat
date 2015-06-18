b485bd5fa736dea8022e0150ca22e99322d197ab
mizuky fujitani <mizuki.fujitani@gmail.com>
2015-06-18 13:28:02 +0900

    ランチャースクリプトのサンプルを追加

---
+++ jp1ajs2-jobextract.bat
@@ -0,0 +1,2 @@
+@echo off
+java -jar %~dp0/target/jp1ajs2-jobextract-0.0.2-SNAPSHOT-jar-with-dependencies.jar %*
---
+++ jp1ajs2-jobextract.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+java -jar `dirname $0`/target/jp1ajs2-jobextract-0.0.2-SNAPSHOT-jar-with-dependencies.jar $*
